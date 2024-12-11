c175173f71368594a2c82d005d68d4752283464a
Adrien Thébault <adrien@thebault.xyz>
2024-12-11 17:05:21 +0100

    day9 p2

--- src/day9.rs
+++ src/day9.rs
@@ -3,7 +3,7 @@ use std::iter;
 
 fn main() {
     let mut fid = -1;
-    let input = String::from_utf8_lossy(include_bytes!("../inputs/day9.txt"))
+    let mut input = String::from_utf8_lossy(include_bytes!("../inputs/day9.txt"))
         .chars()
         .filter_map(|c| c.to_digit(10).and_then(|d| d.try_into().ok()))
         .enumerate()
@@ -20,6 +20,14 @@ fn main() {
         })
         .collect_vec();
 
+    let checksum = |vec: Vec<i32>| {
+        vec.into_iter()
+            .enumerate()
+            .filter(|&(_, n)| n != -1)
+            .map(|(i, n)| i * n as usize)
+            .sum::<usize>()
+    };
+
     // part 1
     let mut p1 = input.clone();
     input
@@ -29,14 +37,32 @@ fn main() {
         .take_while(|&(f, u)| f < u)
         .for_each(|(f, u)| p1.swap(f, u));
 
-    println!(
-        "part 1 : {}",
-        p1.iter()
-            .take_while(|&&n| n != -1)
-            .enumerate()
-            .map(|(i, &n)| i * n as usize)
-            .sum::<usize>()
-    );
+    println!("part 1 : {}", checksum(p1));
 
     // part 2
+    let (free, mut used): (Vec<_>, Vec<_>) = input
+        .iter()
+        .enumerate()
+        .chunk_by(|(_, x)| *x)
+        .into_iter()
+        .map(|(_, chunk)| {
+            let chunk = chunk.collect_vec();
+            (chunk[0].0, chunk.len())
+        })
+        .partition(|&(p, _)| input[p] == -1);
+
+    free.into_iter().for_each(|(f_start, f_len)| {
+        let mut free = f_len;
+        while let Some(p) = used
+            .iter()
+            .rev()
+            .position(|&(u_start, u_len)| free > 0 && u_len <= free && u_start > f_start)
+        {
+            let (u_start, u_len) = used.remove(used.len() - 1 - p);
+            (0..u_len).for_each(|i| input.swap(f_start + (f_len - free) + i, u_start + i));
+            free -= u_len;
+        }
+    });
+
+    println!("part 2 : {}", checksum(input));
 }
